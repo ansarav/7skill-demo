9f7a45ae3687ef5aefc8947cdaba89d385bda6a5
ansarav <ansaravi@ucsd.edu>
2022-02-04 15:27:32 -0800

    Fixed test to pass
    a

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,8 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void Subtract() {
-        assertEquals(3, SkillDemo.Subtract(4,1));// purposefully make it fail
-        // that way you can pull and in github 
+        assertEquals(3, SkillDemo.Subtract(4,1));//pass
         
     }
 }
